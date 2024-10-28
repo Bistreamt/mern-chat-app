06d9ce72d93faebed6fa421a73d69c43efaaaad2
Bistreamt <bistreanu_maria@yahoo.com>
2024-10-28 12:52:54 +0200

    socket update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,8 +15,8 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			// const socket = io("https://mern-chat-app-afpi.onrender.com/", {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://mern-chat-app-afpi.onrender.com/", {
+			// const socket = io("http://localhost:5000", {
 				query: {
 					userId: authUser._id,
 				},
